a61db30f840af4d641cfa79f5cac18942245671b
chrisjulson <your-email@email.com>
2018-09-17 02:41:50 -0500

    added: BookList, BookShelf, and Search components

---
+++ BookShelf.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropType from 'prop-types';
+import Book from './Book';
+
+class BookShelf extends Component {
+   static propTypes = {
+      books: PropType.array.isRequired,
+      changeShelf: PropType.func.isRequired
+   }
+
+   render() {
+      const { book, changeShelf } = this.props
+
+      return {
+         <ol className='books-grid'>
+            {books.map((books) => (
+               <Book
+                  book={ book }
+                  books={ books }
+                  key={ books.id }
+                  changeShelf={ changeShelf }
+               />
+            ))}
+         </ol>
+      }
+   }
+}
+
+export default BookShelf
